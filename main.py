f12cad01a8f422ed606b4f35994512e6582b7dd1
Stepan <stepan_boynice@mail.ru>
2022-02-08 02:37:08 +0300

    4th line (full info with name)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print('i love ny!'.upper())
 # n jjj wow
-#git log, git log --pretty=oneline k
+# git log, git log --pretty=oneline k
+# git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short
