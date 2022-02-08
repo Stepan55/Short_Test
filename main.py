41a8c3678784d3c57122fc8fa8dd3f44a721d85a
Stepan <stepan_boynice@mail.ru>
2022-02-08 19:49:31 +0300

    The 6th line - git hist

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ print('i love ny!'.upper())
 # git log, git log --pretty=oneline k
 # git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short
 # alias (alias gs='git status ')
+# git config --global alias.hist "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
