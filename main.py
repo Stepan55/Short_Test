6e01f75a5f1aeabdc50e525a7dbe4767d8c9efd1
Stepan <stepan_boynice@mail.ru>
2022-02-08 23:06:03 +0300

    add and -m

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print('i love ny!'.upper())
 # n jjj wow
+# git add git commit -m 'Text'
 # git log, git log --pretty=oneline k
 # git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short
 # alias (alias gs='git status ')
