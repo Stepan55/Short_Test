e41847e3028d20004c5e726cd8387251efcb9cf7
Stepan <stepan_boynice@mail.ru>
2022-02-08 20:14:59 +0300

    trying text

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print('i love ny!'.upper())
 # git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short
 # alias (alias gs='git status ')
 # git config --global alias.hist "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+#some text
